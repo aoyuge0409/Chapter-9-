2e8801aee2433b98180b0ed589cc3849c36857aa
Andressa Yuge <yuriyuge04@gmail.com>
2020-02-28 19:16:21 -0500

    Chapter 9

---
+++ Chapter 9 exerc 3/Chapter 9 exerc 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Chapter_9_exerc_3
+{
+    class MyClass
+
+    {
+        public int val;
+    }
+    struct myStruct
+    {
+        public int val;
+
+    }
+    class program
+    {
+        static void Main (string[] args)
+        {
+            MyClass objectA = new MyClass();
+            MyClass objectAB = objectA;
+            objectA.val = 10;
+            objectB.val = 20;
+            myStruct structA = new myStruct();
+            myStruct structB = structA;
+            structA.val = 30;
+            structB.val = 40;
+            Console.WriteLine("objectA.val = {0", objectA.val);
+            Console.WriteLine("objectB.val = {0", objectB.val);
+            Console.WriteLine("objectA.val = {0", structA.val);
+            Console.WriteLine("objectB.val = {0", structB.val);
+            Console.ReadKey();
+
+        }
+    }
+      
+        
+            
+        
+    }
+}
